90b9107858086556137ae26a6fd5820e423c6a47
Marko Milosevic <markom@niri.rs>
2021-03-17 15:08:37 +0100

    Add more messages

--- src/main/java/rs/ac/ni/pmf/HelloWorld.java
+++ src/main/java/rs/ac/ni/pmf/HelloWorld.java
@@ -4,6 +4,8 @@ public class HelloWorld
 {
 	public static void main(String[] args)
 	{
-		System.out.println("Hello Git!");
+		System.out.println("Hello Git!!!");
+		System.out.println("Some serious changes to the code");
+		System.out.println("And more changes...");
 	}
 }
